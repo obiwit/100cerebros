8cfeb1ee0223dd6568ae7fda8dcc359e016db488
pedrovt <pedro.teix@ua.pt>
2017-03-07 21:42:04 +0000

    (LSD) Corrigido Guião 3
    Correcção de erro na parte 3; split of part 2, 3 and 4 in 3 different projects

--- LSD/G3/pedrovt/Parte1/AddSub4.vhd
+++ LSD/G3/pedrovt/Parte1/AddSub4.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
 entity AddSub4 is 
 	port(a, b : in  std_logic_vector(3 downto 0);
@@ -8,20 +9,20 @@ entity AddSub4 is
 		  s	 : out std_logic_vector(3 downto 0));
 end AddSub4;
 
-architecture Structural of AddSub4 is
-	signal s_b : std_logic_vector (3 downto 0);
-
-begin
-	--s_b <= b when (sub = '0') else not b;
-	s_b <= not b when (sub = '1') else b;
-	
-	add_4 : entity work.Adder4(Structural)
-		port map(a    => a,
-					b 	  => s_b,
-					cin  => sub,
-					cout => cout,
-					s    => s);
-end Structural;
+--architecture Structural of AddSub4 is   --está a funcionar?
+--	signal s_b : std_logic_vector (3 downto 0);
+--
+--begin
+--	--s_b <= b when (sub = '0') else not b;
+--	s_b <= not b when (sub = '1') else b;
+--	
+--	add_4 : entity work.Adder4(Structural)
+--		port map(a    => a,
+--					b 	  => s_b,
+--					cin  => sub,
+--					cout => cout,
+--					s    => s);
+--end Structural;
 
 
 architecture Behavioral of AddSub4 is

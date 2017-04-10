3d0f02a8a7c88f5e0a6c03060ffdce87e1469dc6
pedrovt <pedro.teix@ua.pt>
2017-04-10 12:43:57 +0100

    (pedro : LABI) Adicionado Guião 18

---
+++ LABI/G18/pedrovt/ex18_11.py
@@ -0,0 +1,29 @@
+'''
+Created on 09/04/2017
+
+@author: Pedro
+'''
+
+import sqlite3 as sql
+import sys
+
+def main(argv):
+    db = sql.connect(argv[1])
+    
+    name = str(raw_input("Name? "))
+    
+    # if on the table contacts, instead of name we had firstname and surname columns
+    # result = db.execute("SELECT * FROM contacts WHERE firstname LIKE ? OR middlename LIKE ? ", (name,))
+    
+    result = db.execute("SELECT * FROM contacts WHERE name LIKE ? ", (name,))
+    rows = result.fetchall()
+    
+    count = 0
+    for row in rows:
+        count = count + 1
+        print row
+    
+    print("Search finished. Found %d contacts. " %count)
+    db.close()
+    
+main(sys.argv)

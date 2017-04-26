8cf49252a7d1f55f22671f0d84c44d0332a85002
pedrovt <pedro.teix@ua.pt>
2017-04-26 21:05:27 +0100

    (pedro : P2) ea, ex LinkedList

---
+++ P2/pedrovt/GuiaoTA/e09/p2utils/Node.java
@@ -0,0 +1,20 @@
+package e09.p2utils;
+
+class Node<E> {
+
+   final E elem;
+   Node<E> next;
+
+   Node(E e, Node<E> n) {
+      elem = e;
+      next = n;
+   }
+
+   Node(E e) {
+      elem = e;
+      next = null;
+   }
+
+}
+
+
--- P2/pedrovt/GuiaoTA/pg2/E6.java
+++ P2/pedrovt/GuiaoTA/pg2/E6.java
@@ -19,31 +19,36 @@ public class E6 {
 		if (args.length < 1) {
 			System.out.println("Please insert the number: ");
 			int num = read.nextInt(); 
-			System.out.println(num);
-			divisors(num);
+			//System.out.println(num);
+			divisors(num, 0);
 		}
 		// Or from console
 		else {
 			for (String i : args) {
-				System.out.println(i);
-				divisors(Integer.parseInt(i)); 
+				//System.out.println(i);
+				divisors(Integer.parseInt(i), 0); 
 			}
 		}
 	}
 	
 	// It's working
-	public static void divisors (int argNum) {
+	public static void divisors (int argNum, int numSpaces) {
+		
+		String spaces = "";
+		
+		for (int i = 0; i < numSpaces; i++) {
+			spaces += "   ";
+		}
+		System.out.println(spaces + argNum);
 		/* Can't be the number nor number 1; i = argNum-1 and not 0 
 		 * because the output in the question shows from the greater diviser 
 		 * to the smaller */
-		
 		for (int i = argNum - 1 ; i > 1; i--) {			
 			if (argNum % i == 0) {
-				System.out.print("  " + i);
 				// System.out.println("  DIVISORS FROM " + i);
-				divisors(i);
+				divisors(i, numSpaces+1);
 				// System.out.println("  END DIVISORS FROM " + i);
-				System.out.println();
+			
 			}
 			
 		}

d2702ece92bd7b14e2e270e73d78f3c2d5873216
Beatriz Borges <beatrizmbribeiro@gmail.com>
2017-05-19 11:13:50 +0100

    (bea) P2 - Guiao 11, feito, yeay! (/◔ ◡ ◔)/

---
+++ P2/bea/G11/p2utils/LinkedList.java
@@ -0,0 +1,91 @@
+package p2utils;
+
+public class LinkedList<E> {
+
+  private Node<E> first = null;
+  private Node<E> last = null;
+  private int size = 0;
+
+  /**
+   * {@code LinkedList} constructor, empty so far.
+   */
+  public LinkedList() { }
+
+  /** Returns the number of elements in the list.
+   * @return Number of elements in the list
+   */
+  public int size() { return size; }
+
+  /** Checks if the list is empty.
+   * @return  {@code true} if list empty, otherwise {@code false}.
+   */
+  public boolean isEmpty() { return size == 0; }
+
+  /** Returns the first element in the list.
+   * @return  First element in the list
+   */
+  public E first() {
+    assert !isEmpty(): "empty!";
+
+    return first.elem;
+  }
+
+  /** Returns the last element in the list.
+   * @return Last element in the list
+   */
+  public E last() {
+    assert !isEmpty(): "empty!";
+
+    return last.elem;
+  }
+
+  /** Adds the given element to the start of the list.
+   * @param e the element to add
+   */
+  public void addFirst(E e) {
+    first = new Node<>(e, first);
+    if (isEmpty())
+      last = first;
+    size++;
+
+    assert !isEmpty(): "empty!";
+    assert first().equals(e) : "wrong element";
+  }
+
+  /** Adds the given element to the end of the list.
+   * @param e the element to add
+   */
+  public void addLast(E e) {
+    Node<E> newest = new Node<>(e);
+    if (isEmpty())
+      first = newest;
+    else
+      last.next = newest;
+    last = newest;
+    size++;
+
+    assert !isEmpty(): "empty!";
+    assert last().equals(e) : "wrong element";
+  }
+
+  /** Removes the first element in the list.
+   */
+  public void removeFirst() {
+    assert !isEmpty(): "empty!";
+    first = first.next;
+    size--;
+    if (isEmpty())
+      last = null;
+  }
+
+  /** Removes all elements.
+   */
+  public void clear() {
+    first = last = null;
+    size = 0;
+  }
+
+  // Acrescente as funções adicionais pedidas no guião...
+
+
+}

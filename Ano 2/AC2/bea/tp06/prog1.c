af85dd1b7468f42a1ab1f2a8e5871533acd0be79
Beatriz Borges <beatrizmbribeiro@gmail.com>
2018-03-20 13:21:23 +0000

    AC2 - G6, ex 1 and 2

---
+++ Ano 2/AC2/bea/tp06/prog1.c
@@ -0,0 +1,73 @@
+#include <detpic32.h>
+
+// Interrupt Handler
+// void _int_(VECTOR) isr_adc(void) // Replace VECTOR by the A/D vector
+// {
+//    // ISR actions
+//    (...)
+//    IFS1bits.AD1IF = 0; // Reset AD1IF flag
+// }
+
+
+// int main(void)
+// {
+// 	IPC6bits.AD1IP = 2; 	// configure priority of A/D interrupts
+// 	IEC1bits.AD1IE = 1; 	// enable A/D interrupts
+// 	IFS1bits.AD1IF = 0; 	// clear A/D interrupt flag
+// 	// ?? enable interruptions (ei)?
+// }
+
+void main(void)
+{
+	// Configure all (digital I/O, analog input, A/D module)
+
+	// Configure the A/D module and port RB4 as analog input
+	TRISBbits.TRISB4 = 1; 		// RB4 digital output disconnected 
+	AD1PCFGbits.PCFG4 = 0; 		// RB4 configured as analog input (AN4)
+
+	AD1CON1bits.SSRC = 7;		// Conversion trigger selection bits: in this 
+								// mode an internal counter ends sampling and 
+								// starts conversion 
+
+	AD1CON1bits.CLRASAM = 1;	// Stop conversions when the 1st A/D converter 
+								// interrupt is generated. At the same time, 
+								// hardware clears the ASAM bit 
+
+	AD1CON3bits.SAMC = 16;		// Sample time is 16 TAD (TAD = 100 ns) 
+
+	AD1CON2bits.SMPI = 0;		// Interrupt is generated after 1=(0+1) sample
+
+	AD1CHSbits.CH0SA = 4; 		// Selects AN4 as input for the A/D converter
+
+	AD1CON1bits.ON = 1;			// Enable A/D converter 
+								// This must the last command of the A/D 
+								// configuration sequence
+	
+	IPC6bits.AD1IP = 2; 	// configure priority of A/D interrupts
+	IEC1bits.AD1IE = 1; 	// enable A/D interrupts
+	IFS1bits.AD1IF = 0; 	// clear A/D interrupt flag
+ 	
+	// Configure interrupt system
+	IFS1bits.AD1IF = 0;		// Reset AD1IF flag
+	EnableInterrupts();		// Global Interrupt Enable
+
+	// Start A/D conversion
+	AD1CON1bits.ASAM = 1;
+
+	while(1) { }			// all activity is done by the ISR
+
+}
+
+// Interrupt Handler
+void _int_(27) isr_adc(void) {
+
+	// Print ADC1BUF0 value		// Hexadecimal (3 digits format)
+	printInt(ADC1BUF0, 16 | 3 << 16);
+
+	putChar('\n');  // esthetic reasons
+
+	AD1CON1bits.ASAM = 1; 			// Start conversion
+	IFS1bits.AD1IF = 0;				// Reset AD1IF
+}
+
+
